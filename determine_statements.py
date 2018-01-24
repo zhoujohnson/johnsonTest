5dcc3f4ce1fa4d2fc5b6b40d35ac3953d140b053
DESKTOP-NIVLAII\97101 <zhouzeqiang>
2018-01-24 10:01:13 +0800

    没啥好说的
    我也是

---
+++ determine_statements.py
@@ -0,0 +1,11 @@
+t = input("your age:")
+age = int(t)
+if age>18:
+    print ("your age is:",age)
+    print ("you are adult")
+elif age>6:
+    print ("your age is :",age)
+    print ("you are a teenage")
+else :
+    print ("your age is :",age)
+    print ("you are a kid")
